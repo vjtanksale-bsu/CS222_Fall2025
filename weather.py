cf0a031f8e6da10b298d59aab1e1fe1594604326
vjtanksale-bsu <141774588+vjtanksale-bsu@users.noreply.github.com>
2025-09-09 12:09:22 -0400

    JSON Python

---
+++ weather.py
@@ -0,0 +1,14 @@
+# https://api.weather.gov/alerts/active?area=IN
+import json
+import ssl
+from urllib.request import urlopen
+def main():
+    state = input("Enter two-character state code: ")
+    url = "https://api.weather.gov/alerts/active?area=" + state
+    context = ssl._create_unverified_context()
+    response = urlopen(url, context=context)
+    weatherData = json.loads(response.read())
+    print(len(weatherData["features"]))
+    for event in weatherData["features"]:
+        print(event["properties"]["headline"])
+main()
